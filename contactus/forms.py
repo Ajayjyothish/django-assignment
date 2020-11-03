d6b69763f0a46889925b266bbe1650d47fbc860d
ajayj <ajayj@qburst.com>
2020-11-03 13:29:12 +0530

    Forms.py added 1.Name validation satisfied 2.Email validation satisfied 3.Phone validation satisfied

---
+++ contactus/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from . import models
+
+class ContactForm(forms.ModelForm):
+
+    name = forms.CharField(min_length=3, max_length=100)
+    phone= forms.IntegerField(required=False, min_value=1000000000, max_value=9999999999)
+    class Meta:
+        model = models.Contact
+        fields = ['name','email', 'phone', 'description']
