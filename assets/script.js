15138005da201bb2db3a89be5c3096f00894caab
ajayj <ajayj@qburst.com>
2020-11-04 13:56:37 +0530

    jquery validation added

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,47 @@
-$(document).on("submit", "#post-form", function (e) {
-  e.preventDefault();
-  $.ajax({
-    type: "POST",
-    url: "",
-    data: {
-      name: $("#id_name").val(),
-      email: $("#id_email").val(),
-      phone: $("#id_phone").val(),
-      description: $("#id_description").val(),
-      csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
-      action: "post",
+$("#post-form").validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 5,
     },
-    success: function (json) {
-      document.getElementById("post-form").reset();
-      alert("success");
+    email: {
+      required: true,
+      email: true,
     },
-    error: function (xhr, errmsg, err) {
-      console.log(xhr.status + ": " + xhr.resposeText);
+    phone: {
+      minlength: 10,
+      maxlength: 10,
+      number: true,
     },
-  });
+  },
+  messages: {
+    name: {
+      required: "Please put your full name.",
+      minlength: "C'mon full name please.",
+    },
+    email: "A valid email please",
+    phone: "Enter a 10 digit mobile number",
+  },
+  submitHandler: function (form, event) {
+    event.preventDefault();
+    $.ajax({
+      type: form.method,
+      url: "",
+      data: {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        phone: $("#phone").val(),
+        description: $("#description").val(),
+        csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+        action: "post",
+      },
+      success: function (json) {
+        document.getElementById("post-form").reset();
+        alert("success");
+      },
+      error: function (xhr, errmsg, err) {
+        console.log(xhr.status + ": " + xhr.resposeText);
+      },
+    });
+  },
 });
